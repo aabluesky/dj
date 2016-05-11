6c43189fafc9cf78c64455f682a7a6b7a4fb7d80
xxb <xxb>
2016-05-11 14:14:18 +0100

    f

---
+++ dj/blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+	url(r'^$',views.home,name='home'),
+	url(r'^(?P<id>\d+)/$',views.detail,name='detail'),
+	url(r'^archives/$',views.archives,name='archives'),
+	url(r'^aboutme/$',views.about_me,name='about_me'),
+	url(r'^tag(?P<tag>\w+)/$',views.search_tag, name = 'search_tag'),
+	url(r'^search/$',views.blog_search, name = 'search'),
+]
